70d4294f54f46238d5552b7a4e120fe12af9e9e3
RayPerez22 <raynyc2285@gmail.com>
2022-06-24 22:03:03 -0400

    first commit

---
+++ space.js
@@ -0,0 +1,35 @@
+//ship class that will take the blueprint to make the USS Assembly and the alien ship
+class Ship {
+    constructor(hull,firepower,accuracy){
+        this.hull = hull
+        this.firepower = firepower
+        this.accuracy = accuracy
+    }  
+}
+   
+
+//Create a human ship class that will take atributes form parent class. With these attributes(hull - 20, firepower - 5, accuracy - .7)
+const human = new Ship() 
+//make a method to attack and retreat.
+//-figure out a way to deduct damege        
+//-after you get hit fire back
+//-make a method that the ship is destroyed
+
+///////////////////////////////////////////////
+
+
+//Create alien ship class that will take atributes form parent class. With these attributes (hull - between 3and 6, firepower - between 2 and 4, accuracy - between .6 and .8)
+const alien = new Ship()
+//-make a method to attack and retreat.
+//-figure out a way to deduct damege.
+//-after you get hit fire back
+//-make a method that the ship is destroyed
+
+
+
+//instance of each class
+
+
+
+
+
